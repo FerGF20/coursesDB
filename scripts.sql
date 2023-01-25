47099df88a0e7f767a651d6f1029715896506c67
FerGF20 <fernando55021@hotmail.com>
2023-01-25 16:40:22 -0500

    first commit

---
+++ scripts.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users (id,"name",email,"password",age) VALUES
+	 ('0321af91-1836-4c1c-89c8-a2736b8784af','Alex Carranza','alex0808@academlo.com','root',19),
+	 ('f4a143c4-cc92-4b42-b56f-c0e6b5084f23','Oliver Fabri','oliver05@academlo.com','root',18);
